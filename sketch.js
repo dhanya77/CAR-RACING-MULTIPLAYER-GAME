7f1a5ed25f3ed1a4ff9e6a7de6cd106f9583b590
dhanya77 <76441654+dhanya77@users.noreply.github.com>
2021-04-28 17:05:26 +0530

    Add files via upload
    I COMMIT MY CHANGES

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var hball, database
+var position  
+function setup(){
+    database=firebase.database()
+    console.log(database)
+    createCanvas(500,500);
+    hball = createSprite(250,250,10,10);
+    hball.shapeColor = "red";
+    
+    var hballposition=database.ref('ball/position')
+    hballposition.on("value",readPosition,showError)
+
+
+}
+
+function draw(){
+    background("white");
+    
+
+    
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x ,
+        'y':position.y+y
+    })
+
+
+}
+
+function readPosition(data){
+position=data.val()
+console.log(position.x)
+hball.x=position.x
+hball.y=position.y
+}
+
+
+
+function showError(){
+    console.log("Error")
+}
